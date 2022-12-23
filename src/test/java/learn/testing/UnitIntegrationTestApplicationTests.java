d22558c97d35ab796f941b620e90dc8018a69e89
soochan <tncksdl05@gmail.com>
2022-12-23 11:49:42 +0900

    junit5을 이용한 간단한 유닛 테스트

--- src/test/java/learn/testing/UnitIntegrationTestApplicationTests.java
+++ src/test/java/learn/testing/UnitIntegrationTestApplicationTests.java
@@ -1,13 +1,33 @@
 package learn.testing;
 
+
+import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UnitIntegrationTestApplicationTests {
 
+	Calculator calculator = new Calculator();
+
 	@Test
-	void contextLoads() {
+	void itShouldAddTwoNumbers() {
+		// given
+		int numberOne = 20;
+		int numberTwo = 30;
+
+		// when
+		int result = calculator.add(numberOne, numberTwo);
+
+		// then
+		int expected = 50;
+		assertThat(result).isEqualTo(expected);
 	}
 
+
+	class Calculator {
+		public int add(int a, int b) {
+			return a + b;
+		}
+	}
 }
