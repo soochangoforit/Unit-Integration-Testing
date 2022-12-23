15c6035421e15c1be23cc68ecbef264f23fc2c05
soochan <tncksdl05@gmail.com>
2022-12-23 11:39:56 +0900

    initial commit

---
+++ src/main/java/learn/testing/UnitIntegrationTestApplication.java
@@ -0,0 +1,13 @@
+package learn.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitIntegrationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitIntegrationTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/learn/testing/UnitIntegrationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitIntegrationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
