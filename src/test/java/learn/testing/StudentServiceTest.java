829049e90062aa2655359e89043817adea35f98a
soochan <tncksdl05@gmail.com>
2022-12-23 19:46:04 +0900

    [Test] addStudent method 테스트

--- src/test/java/learn/testing/StudentServiceTest.java
+++ src/test/java/learn/testing/StudentServiceTest.java
@@ -1,11 +1,13 @@
 package learn.testing;
 
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -30,8 +32,31 @@ class StudentServiceTest {
         verify(studentRepository).findAll();
     }
 
+    /**
+     * 학생이 성공적으로 추가되었는지 확인
+     */
     @Test
-    void addStudent() {
+    void canAddStudent() {
+        // given
+        Student student = new Student(
+            "James",
+            "james@gmail.com",
+            Gender.FEMALE
+        );
+
+        //when
+        studentService.addStudent(student);
+
+        //then
+        ArgumentCaptor<Student> studentArgumentCaptor = ArgumentCaptor.forClass(Student.class);
+
+        // studentRepository.save(student)로 넘어온 student를 캡쳐한다
+        verify(studentRepository).save(studentArgumentCaptor.capture());
+
+        Student capturedStudent = studentArgumentCaptor.getValue();
+
+        // 캡쳐한 결과 값이 student와 같은지 확인
+        assertThat(capturedStudent).isEqualTo(student);
     }
 
     @Test
