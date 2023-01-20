5db8aec99e4538a3f587cb33b11aa254c1cebc7e
soochan <tncksdl05@gmail.com>
2023-01-20 17:09:29 +0900

    [Test] delete some test code

--- src/test/java/learn/testing/UnitIntegrationTestApplicationTests.java
+++ src/test/java/learn/testing/UnitIntegrationTestApplicationTests.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class UnitIntegrationTestApplicationTests {
 
 	Calculator calculator = new Calculator();
