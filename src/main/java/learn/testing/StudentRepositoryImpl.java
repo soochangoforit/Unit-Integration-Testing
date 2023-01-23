d6c58ee4559c90cb2c2d9cd8a03632390c67ea84
soochan <tncksdl05@gmail.com>
2023-01-24 05:24:31 +0900

    test 3

--- src/main/java/learn/testing/StudentRepositoryImpl.java
+++ src/main/java/learn/testing/StudentRepositoryImpl.java
@@ -4,17 +4,20 @@ import static learn.testing.QStudent.student;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
+import javax.persistence.EntityManager;
+
 
 /**
  * querydsl을 사용하여 동적쿼리를 구현하는 클래스.
  */
-@RequiredArgsConstructor
-@Configuration
+
 public class StudentRepositoryImpl implements StudentRepositoryCustom {
 
   private final JPAQueryFactory queryFactory;
+
+  public StudentRepositoryImpl(EntityManager em) {
+    this.queryFactory = new JPAQueryFactory(em);
+  }
 
   @Override
   public Student findStudentByEmail(String email) {
