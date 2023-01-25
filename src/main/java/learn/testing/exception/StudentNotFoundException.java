62eb8df3a1fad7413941d72a84ff7854dee71814
soochan <tncksdl05@gmail.com>
2023-01-25 15:11:11 +0900

    test 10

--- src/main/java/learn/testing/exception/StudentNotFoundException.java
+++ src/main/java/learn/testing/exception/StudentNotFoundException.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class StudentNotFoundException extends RuntimeException {
+
     public StudentNotFoundException(String message) {
       super(message);
     }
